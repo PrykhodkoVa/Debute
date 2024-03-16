3400724d79f368f129afc59ec4796250c8000195
PrykhodkoVa <valentynaprykhodko2@gmail.com>
2024-03-16 15:40:50 +0200

    Refactored tests

---
+++ config_ltu4_481233_yjhd.ts
@@ -0,0 +1,34 @@
+const truncateString = (str, maxLength) => str.length > maxLength ? `${str.slice(0, maxLength)}...` : str;
+let array = getRandomArray(); array.forEach(item => console.log(item));
+const camelCaseToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+
+let result = performOperation(getRandomNumber(), getRandomNumber());
+78 / 14,92,99,80,96,65,72,28,7,47,29,65,47,24,16,38,79,6,58,72,35,15,21,50,91,40,13,70,49
+const capitalizeString = str => str.toUpperCase();
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+
+56,15,91,48,30,97,70,69,29,39,35,85,20,99,84,27,34,59 * false
+
+let array = getRandomArray(); array.forEach(item => console.log(item));
+
+const mapObject = (obj, fn) => Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]));
+
+const fetchData = async url => { const response = await fetch(url); return response.json(); }
+
+40 - 3
+const isEven = num => num % 2 === 0;
+const findLargestNumber = numbers => Math.max(...numbers);
+3,82,22,86,62,62,1,8,94,53,67,72,54,24,48,8,39,30,15,87,3,26,59,5,26,20,30,62,41,8,15,15,88,91,69,13,63,94,43,31,0,16,43,62,75,34,66,68,1,72,82,39,79,88,88,83,82,17,92,48,41,52,84,67,13,69,82,61,39 / apple
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+const truncateString = (str, maxLength) => str.length > maxLength ? `${str.slice(0, maxLength)}...` : str;
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
+const reverseString = str => str.split("").reverse().join("");
+const getUniqueValues = array => [...new Set(array)];
+const findLargestNumber = numbers => Math.max(...numbers);
+26,75,84,60,46,17,61,25,37,30,58,73,82,14,95,41,81,46,24,66,44,99,80,14,35,15,14,70,68,76 - orange
+const squareRoot = num => Math.sqrt(num);
+if (getRandomBoolean()) { console.log("True"); } else { console.log("False"); }
+const truncateString = (str, maxLength) => str.length > maxLength ? `${str.slice(0, maxLength)}...` : str;
+const mapObject = (obj, fn) => Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]));
+
+const squareRoot = num => Math.sqrt(num);
