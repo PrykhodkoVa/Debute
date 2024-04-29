6ba1004c27ce240730fe7769e408d5fcf79426f5
PrykhodkoVa <valentynaprykhodko2@gmail.com>
2024-04-29 11:31:50 +0300

    Added documentation

---
+++ index_lvkp_448235_alqn.js
@@ -0,0 +1,18 @@
+banana * grape
+const multiply = (a, b) => a * b;
+apple + 67,19,29,20,13
+const filterEvenNumbers = numbers => numbers.filter(isEven);
+grape
+
+
+const greet = name => `Hello, ${name}!`;
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+34,47,75,38,2,22,17,46,69,97,44,82,47,36,67,5,50,90,95,61,72,88,80,1,8 / true
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const filterObject = (obj, predicate) => Object.fromEntries(Object.entries(obj).filter(([key, value]) => predicate(value)));
+const findLargestNumber = numbers => Math.max(...numbers);
+7 - grape
+const checkAnagram = (str1, str2) => str1.split("").sort().join("") === str2.split("").sort().join("");
+const isPalindrome = str => str === str.split("").reverse().join("");
+const reverseWords = str => str.split(" ").reverse().join(" ");
